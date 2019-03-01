2c236a6d711018d76824507e1b7dbc95cbe08791
zukirou <zukiroudev@gmail.com>
2019-03-01 12:53:18 +0900

    Assets\Objectno

---
+++ Energy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Energy : MonoBehaviour {
+
+    public GameObject particle;
+
+    GameObject VariousCount;
+    VariousCount variousCount;
+
+    GameObject ProgressManager;
+    GameProgress gameProgress;
+
+	void Start () {
+        VariousCount = GameObject.Find("CountManager");
+        variousCount = VariousCount.GetComponent<VariousCount>();
+
+        ProgressManager = GameObject.Find("ProgressManager");
+        gameProgress = ProgressManager.GetComponent<GameProgress>();
+    }
+	
+
+    void OnCollisionEnter(Collision collision) {
+        if(collision.gameObject.tag == "Player") {
+
+            //energy獲得時のエフェクト発生
+            Instantiate(particle, transform.position, transform.rotation);
+
+            //消す
+            Destroy(this.gameObject);
+
+            //カウントアップ
+            variousCount.countGetEnergy += 10;
+            variousCount.CountGetEnergyText.text = variousCount.countGetEnergy.ToString();
+
+            //全部のエネルギーをゲットしたかチェック
+            if(variousCount.countGetEnergy == variousCount.countAllEnergy) {
+                gameProgress.phase = GameProgress.Phase.energyAllGet;
+            }
+        }
+    }
+
+}
---
+++ MouseInput.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseInput : MonoBehaviour {
+
+    [Header("Input Settings:")]
+    public float pressureForce;
+
+    public RaycastHit hitInfo;
+    public Ray ray;
+
+    public Vector3 inputPoint;
+    public JellyBody jellyBody;
+    public GameObject jelly;
+
+    public JellyBody.Status jellyBodyStatus;
+
+    public static MouseInput instance;
+
+    public GameObject BattleCamera;
+    public Camera battleCamera;
+
+    public int energyID;
+
+	void Start () {
+        instance = this;
+        pressureForce = 20f;
+
+        energyID = 0;
+
+        BattleCamera = GameObject.Find("BattleCamera");
+        battleCamera = BattleCamera.GetComponent<Camera>();
+
+        jellyBodyStatus = JellyBody.Status.released;//これ、つかってなくね？
+	}
+
+
+    public void Update() {
+        CheckForTouch();
+        GetEnergyID();
+        jellyBodyStatus = JellyBody.Status.released;
+    }
+
+
+    public int GetEnergyID() {
+        return energyID;
+    }
+
+    public void ResetEnergyID() {
+        energyID = 0;
+    }
+
+    public void CheckForTouch() {
+        if (Input.GetMouseButton(0)) {
+            ray = battleCamera.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out hitInfo)) {
+                jellyBody = hitInfo.collider.gameObject.GetComponent<JellyBody>();
+                if(jellyBody != null) {
+                    inputPoint = hitInfo.point;
+                    inputPoint += hitInfo.normal * 0.1f;
+                    jellyBody.AddPointForce(pressureForce, inputPoint);
+
+                    //どのエネルギーにタッチしたか、そのIDを返す
+                    //IDは個々にpublicで設定
+                    jellyBodyStatus = JellyBody.Status.touched;
+                    energyID = jellyBody.id;
+                }
+            }
+        }
+    }
+}
+
